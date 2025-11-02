da195b6e368fa1a3ddf247b0fdf2ecc6352282c1
Joe Danziger <joe@ticc.net>
2025-11-02 10:28:12 -0500

    Add research_tools_enabled field to API request structs

--- packages/api/src/ideate_handlers.rs
+++ packages/api/src/ideate_handlers.rs
@@ -31,6 +31,8 @@ pub struct StartIdeateRequest {
     pub mode: IdeateMode,
     #[serde(rename = "templateId")]
     pub template_id: Option<String>,
+    #[serde(rename = "researchToolsEnabled")]
+    pub research_tools_enabled: Option<bool>,
 }
 
 /// Start a new ideateing session
@@ -89,6 +91,8 @@ pub struct UpdateIdeateRequest {
     pub status: Option<IdeateStatus>,
     #[serde(rename = "skippedSections")]
     pub skipped_sections: Option<Vec<String>>,
+    #[serde(rename = "researchToolsEnabled")]
+    pub research_tools_enabled: Option<bool>,
 }
 
 /// Update a ideateing session
