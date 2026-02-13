63693e8bc4b19d110be26f44a08038c1223c9644
Joe Danziger <joe@ticc.net>
2026-02-13 10:23:42 -0500

    refactor: remove dead code from old PRD creation modes
    Delete Quick Mode, Guided Mode, PRD Generator, mode/template selectors,
    IdeateTab, SessionsList, ChatModeFlow, and heavy sidebar components.
    These are all replaced by the unified PRDChatFlow component.
    
    Kept: ChatView, MessageBubble, SuggestedQuestions, useChat,
    useStreamingResponse, and constants (still used by PRDChatFlow).

--- packages/dashboard/src/components/ideate/GuidedMode/index.ts
+++
@@ -1,7 +0,0 @@
-// ABOUTME: GuidedMode component exports
-// ABOUTME: Central export point for all GuidedMode components
-
-export { GuidedModeFlow } from './GuidedModeFlow';
-export { SectionNavigator } from './SectionNavigator';
-export { SectionProgress } from './SectionProgress';
-export { SkipDialog } from './SkipDialog';
--- packages/dashboard/src/components/ideate/QuickMode/index.ts
+++
@@ -1,6 +0,0 @@
-export { QuickModeFlow } from './QuickModeFlow';
-export { OneLineInput } from './OneLineInput';
-export { SectionSelector } from './SectionSelector';
-export { GeneratingState } from './GeneratingState';
-export { PRDEditor } from './PRDEditor';
-export { SavePreview } from './SavePreview';
